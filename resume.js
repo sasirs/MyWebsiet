5196e74683ff24600106933f1df0d886a8a4a051
sasikumar <108642303+sasirs@users.noreply.github.com>
2023-11-19 19:43:04 +0530

    Update resume.js

--- resume.js
+++ resume.js
@@ -1,1 +1,1 @@
-var resume_link = "https://drive.google.com/file/d/1jpB4aDi0PJUkBcoTUydYAbjDL0tnVWVo/view?usp=drive_link";
+var resume_link = "https://drive.google.com/file/d/1IOgMs0QEgi-68AksKNGI2MECd3I20bt-/view?usp=drive_link";
